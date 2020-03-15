3c9086bfabbc33f0df5071b2569891a471d87ab6
Russkazkin <ruslan@skazkin.su>
2020-03-15 08:23:58 +0300

    Add categories index route and action

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Category;
 use Illuminate\Http\Request;
 
 class CategoriesController extends Controller
@@ -9,5 +10,10 @@ class CategoriesController extends Controller
     public function __construct()
     {
         $this->middleware('auth');
+    }
+
+    public function index()
+    {
+        return view('categories.index')->with('categories', Category::all());
     }
 }
