619ce078e941855f9c8c3a8de5ca18efd145c1a3
Russkazkin <ruslan@skazkin.su>
2020-03-15 09:21:50 +0300

    Add category create form

--- resources/views/categories/create.blade.php
+++ resources/views/categories/create.blade.php
@@ -6,6 +6,37 @@
 
 @section('content')
 <div class="container">
-
+    <h1>Create category</h1>
+    <div class="row">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">
+                    Create new category
+                </div>
+                <div class="card-body">
+                    @if($errors->any())
+                        <div class="alert alert-danger">
+                            <ul class="list-group">
+                                @foreach($errors->all() as $error)
+                                    <li class="list-group-item">
+                                        {{ $error }}
+                                    </li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endif
+                    <form action="/store-category" method="POST">
+                        @csrf
+                        <div class="form-group">
+                            <input type="text" class="form-control" name="name" placeholder="Name">
+                        </div>
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-success">Create category</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 @endsection
