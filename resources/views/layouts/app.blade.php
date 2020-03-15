87ae0e1f4ecb7e460d982bff34294d379810a3fb
Russkazkin <ruslan@skazkin.su>
2020-03-15 08:36:45 +0300

    Add links to layout navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,7 +33,12 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-
+                        <li class="nav-item active">
+                            <a class="nav-link" href="/home">Home <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/categories/index">Categories</a>
+                        </li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
